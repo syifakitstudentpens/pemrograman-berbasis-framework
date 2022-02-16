c45f3fe523d2444b39e208a53b2ca3c45a79c456
syifakitstudentpens <9a.35syifak@gmail.com>
2022-02-16 12:04:44 +0700

    origin master

---
+++ destructuring.js
@@ -0,0 +1,8 @@
+let polStudent = ({name, polytechnic}) => {
+	alert('${name} from ${polytechic}');
+};
+
+polStudent({
+	name : 'Syifak',
+	polytechnic: 'Politeknik Negeri Malang'
+});
---
+++ spread.js
@@ -0,0 +1,5 @@
+var mountains = ['semeru', 'Bromo', 'Merapi'];
+var mountainsFromJapan = ['Fuji'];
+
+var allMountains = [...mountains, ...mountainsFromJapan];
+alert(allMountains);
